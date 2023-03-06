888344c5acf938f5929b46769f8b0084cc2c2414
Jeronimo Schreyer <jeronimo.schreyer@gmail.com>
2023-03-06 12:05:52 -0300

    refactored image_generation

--- Test/generate_picture.gd
+++ Test/generate_picture.gd
@@ -1,40 +1,41 @@
 tool
 extends Control
 
-onready var response_first_picture = $Background/MarginContainer/VBoxContainer/HBoxContainer/FirstPicture
-onready var response_second_picture = $Background/MarginContainer/VBoxContainer/HBoxContainer/SecondPicture
-onready var response_third_picture = $Background/MarginContainer/VBoxContainer/HBoxContainer/ThirdPicture
-onready var response_fourth_picture = $Background/MarginContainer/VBoxContainer/HBoxContainer/FourthPicture
 
-onready var prompt_text = $Background/MarginContainer/VBoxContainer/HSplitContainer/PromtEdit
-onready var prompt_label = $Background/Prompt_label
-onready var send_button = $Background/MarginContainer/VBoxContainer/HSplitContainer/Button
+const generated_picture = preload("res://Test/generated_picture.tscn")
 
-var current_number : int = 0
+onready var results_container = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer
+onready var prompt_text = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HSplitContainer/PromptEdit
+onready var prompt_label = $MarginContainer/VBoxContainer/Prompt_label
+onready var send_button = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HSplitContainer/Button
+
+
+func _on_main_screen_changed(screen_name: String):
+	if screen_name == "Chat GPT Plugin":
+		prompt_text.grab_focus()
+
 
 func _on_PromtEdit_text_entered(_new_text: String) -> void:
 	_on_Send_Button_pressed()
+
 
 func _on_Send_Button_pressed() -> void:
 	$HTTP.image_generation(prompt_text.text)
 	prompt_label.text = prompt_text.text
 	prompt_text.text = "Generating images..."
-	
-	
-func _on_HTTP_images_request_completed(results) -> void:
-	if len(results) < current_number:
-		$HTTP._on_image_download_request_completed(results)
 
-func _on_HTTP_image_downloaded(texture) -> void:
-	current_number += 1
-	match current_number:
-		1:
-			response_first_picture.texture = texture
-		2:
-			response_second_picture.texture = texture
-		3:
-			response_third_picture.texture = texture
-		4:
-			response_fourth_picture.texture = texture
-			prompt_text.clear()
-			current_number = 0
+
+func _on_HTTP_images_request_completed(results) -> void:
+	prompt_text.clear()
+	# MANERA 1 - GENERANDO LAS IMAGENES EN EL MOMENTO
+#	for generated_texture in results:
+#		var picture = generated_picture.instance()
+#		picture.texture = generated_texture
+#		results_container.add_child(picture)
+
+	# MANERA 2 - UTILIZANDO NODOS PREVIAMENTE CREADOS EN EL EDITOR
+	for i in range(results.size()):
+		if i >= results_container.get_child_count():
+			break
+
+		results_container.get_child(i).texture = results[i]
